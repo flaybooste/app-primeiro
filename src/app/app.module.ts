48a044ed65ac980b987fef059f07b756f0cc5cd4
Luis T <retardado.tw@gmail.com>
2019-04-11 16:12:10 -0300

    Segunda alteração

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { PrimeiroComponentComponent } from './primeiro-component/primeiro-component.component';
@@ -10,7 +11,8 @@ import { PrimeiroComponentComponent } from './primeiro-component/primeiro-compon
     PrimeiroComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent, PrimeiroComponentComponent]
--- src/app/primeiro-component/primeiro-component.component.ts
+++ src/app/primeiro-component/primeiro-component.component.ts
@@ -6,9 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./primeiro-component.component.css']
 })
 export class PrimeiroComponentComponent implements OnInit {
-
+  //filmes: string[] = ["Duro de matar", "Os Vingadores", "Lindo de morrer"];
   constructor() { }
 
+	alerta(texto){
+		fetch(`https://viacep.com.br/ws/${texto}/json/`).then(req => req.json()).then(res => document.getElementById("p1").innerHTML = "Logradouro--> " + res["logradouro"] + "<br>Cidade--> " + res['localidade'] + "<br>UF--> " + res['uf'] + "<br>IBGE--> " + res['ibge']);
+	}
+	
   ngOnInit() {
   }
 
